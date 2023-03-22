af547feade1ce0c564208264d08804052c8fe7df
wz7 <kevin_wz7@163.com>
2023-03-22 14:32:17 +0800

    init:项目初始化

---
+++ main.js
@@ -0,0 +1,14 @@
+import App from './App'
+import uView from 'uview-ui'
+
+// #ifndef VUE3
+import Vue from 'vue'
+
+Vue.use(uView)
+Vue.config.productionTip = false
+App.mpType = 'app'
+const app = new Vue({
+  ...App
+})
+app.$mount()
+// #endif
